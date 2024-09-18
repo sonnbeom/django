7179e4b9b40171bc7feafa154500beb569e8d8c9
eduharryjjun <eduharryjjun@gmail.com>
2024-09-19 00:11:45 +0900

    add 0920

---
+++ 02-template/articles/views.py
@@ -0,0 +1,13 @@
+# import random
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'articles/index.html')
+
+
+def dinner(request):
+    # foods = ['국밥', '국수', '카레', '탕수육',]
+    # picked = random.choice(foods)
+    pass
