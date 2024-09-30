aeb577a3aa1de4bd4987b21276912e6899517b1b
eduharry <harry@forestsoft.co.kr>
2024-09-30 16:01:54 +0900

    done 0930

--- 08-01-authentication/crud/settings.py
+++ 08-01-authentication/crud/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
+    'accounts',
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
