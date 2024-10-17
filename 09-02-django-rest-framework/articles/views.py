cf4ca5524ab359c5be9b6b165d02bbc174685acc
eduharry <harry@forestsoft.co.kr>
2024-10-17 10:58:45 +0900

    done 1017

--- 09-02-django-rest-framework/articles/views.py
+++ 09-02-django-rest-framework/articles/views.py
@@ -2,8 +2,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from .models import Article
-from .serializers import ArticleListSerializer, ArticleSerializer
+from django.shortcuts import get_object_or_404
+
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleSerializer, CommentSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -22,7 +24,8 @@ def article_list(request):
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def article_detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
@@ -39,3 +42,45 @@ def article_detail(request, article_pk):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
+
+@api_view(['GET'])
+def comment_list(request):
+    # 댓글 전체 조회
+    comments = Comment.objects.all()
+    # 댓글 목록 쿼리셋을 직렬화 진행
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    # 단일 댓글 조회
+    comment = Comment.objects.get(pk=comment_pk)
+
+    if request.method == 'GET':
+        # 단일 댓글 직렬화
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+
+@api_view(['POST'])
+def comment_create(request, article_pk):
+    # 게시글 조회 (어떤 게시글에 작성되는 댓글인지)
+    article = Article.objects.get(pk=article_pk)
+    # 사용자 입력 데이터를 직렬화 (사용자가 입력한 댓글 데이터)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # 외래 키 데이터 입력 후 저장
+        serializer.save(article=article)
+        return Response(serializer.data, status=status.HTTP_201_CREATED) 
--- 09-02-django-rest-framework/drf/urls.py
+++ 09-02-django-rest-framework/drf/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include('articles.urls')),
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
